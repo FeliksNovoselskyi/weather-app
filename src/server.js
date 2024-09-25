86d66ba4ca28ec61d1bd25853c5fa06439ed6d36
Novoselskyi Feliks <110721352+FeliksNovoselskyi@users.noreply.github.com>
2024-09-25 18:38:50 +0300

    add static files

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
+// ES-modules import
 import express from 'express'
 import dotenv from 'dotenv'
+
+import {fileURLToPath} from 'url'
+import {dirname, join} from 'path'
 
 dotenv.config({path: '../.env'})
 
@@ -8,8 +12,14 @@ const webApp = express()
 const PORT = process.env.PORT || 3000
 const HOST = process.env.HOST || 'localhost'
 
+// Taking __dirname from __filename
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 webApp.set('view engine', 'ejs')
 webApp.set('views', './templates')
+
+webApp.use('/static/', express.static(join(__dirname, 'static')))
 
 webApp.get('/', (req, res) => {
     res.render('index')
