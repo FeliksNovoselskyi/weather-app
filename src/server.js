097113e5d67740d86662227b61f5f1cee87290ba
Novoselskyi Feliks <110721352+FeliksNovoselskyi@users.noreply.github.com>
2024-09-25 23:54:32 +0300

    add basic comments for server part code

--- src/server.js
+++ src/server.js
@@ -6,11 +6,16 @@ import axios from 'axios'
 import {fileURLToPath} from 'url'
 import {dirname, join} from 'path'
 
+// Config environment variables from directiory before /src/ (directiory of this file)
 dotenv.config({path: '../.env'})
 
 const webApp = express()
 const apiKey = process.env.API_KEY
 
+// Get port and host from environment file
+// If that doesn't work, the alternative is 
+// port - 3000
+// host - localhost
 const PORT = process.env.PORT || 3000
 const HOST = process.env.HOST || 'localhost'
 
@@ -18,16 +23,20 @@ const HOST = process.env.HOST || 'localhost'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
+// Connect templates
 webApp.set('view engine', 'ejs')
 webApp.set('views', './templates')
 
+// Connect static
 webApp.use('/static/', express.static(join(__dirname, 'static')))
 
 webApp.get('/weather', async (req, res) => {
     const city = req.query.city
 
+    // Customed openweathermap api url
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
     
+    // main variables for template declaration
     let weather
     let error = null
 
@@ -44,6 +53,7 @@ webApp.get('/weather', async (req, res) => {
     res.render('index', {weather, error})
 })
 
+// WebApp listening
 webApp.listen(PORT, HOST, () => {
-    console.log(`Сервер запущен на http://${HOST}:${PORT}`)
+    console.log(`Server started on http://${HOST}:${PORT}`)
 })
